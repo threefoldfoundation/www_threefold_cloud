bfb1c5e6c6fb40244f2fe5e85867b4cb4b4ea71c
hamdy <hamdy.a.farag@gmail.com>
2020-12-06 04:27:32 +0200

    release

--- dist/assets/js/page--src--pages--newsroom-vue.5478b2ed.js
+++
@@ -1,1 +0,0 @@
-(window.webpackJsonp=window.webpackJsonp||[]).push([[9],{"/7RI":function(n,t,e){"use strict";e.r(t);var u=e("KHd+"),l=Object(u.a)({},(function(){var n=this.$createElement;return(this._self._c||n)("Layout")}),[],!1,null,null,null);t.default=l.exports}}]);
