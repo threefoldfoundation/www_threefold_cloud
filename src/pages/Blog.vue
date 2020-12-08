c16e1e745d674510840947de98bc903731c43baf
hamdy <hamdy.a.farag@gmail.com>
2020-12-08 03:39:37 +0200

    blog filter

--- src/pages/Blog.vue
+++ src/pages/Blog.vue
@@ -1,12 +1,24 @@
 <template>
   <Layout>
+      <NewsFilterHeader
+      @selectedTopic="setTopic"
+      @selectedYear="setYear"
+      @selectedMonth="setMonth"
+      @resetAll="resetAll"
+      :topics="topics"
+      :years="years"
+      :months="months"
+    />
+    <br/>
+    <br/>
+    
      <div
       class="container sm:pxi-0 mx-auto"
       :style="{ 'min-height': contentHeight + 'px' }"
     >
       <div class="flex flex-wrap with-large pt-8 pb-8 mx-4 sm:-mx-4">
         <PostListItem
-          v-for="post in $page.entries.edges"
+          v-for="post in blogs.edges"
           :key="post.node.id"
           :record="post.node"
         />
@@ -15,10 +27,10 @@
        <div class="pagination flex justify-center mb-8">
       <Pagination
         baseUrl=""
-        :currentPage="$page.entries.pageInfo.currentPage"
-        :totalPages="$page.entries.pageInfo.totalPages"
+        :currentPage="blogs.pageInfo.currentPage"
+        :totalPages="blogs.pageInfo.totalPages"
         :maxVisibleButtons="5"
-        v-if="$page.entries.pageInfo.totalPages > 1"
+        v-if="blogs.pageInfo.totalPages > 1"
       />
     </div>
 
@@ -37,6 +49,11 @@ query{
     edges {
       node {
         title
+        tags{
+          id
+          title
+          path
+        }
         excerpt
         image(width:800)
         path
@@ -51,25 +68,172 @@ query{
       }
     }
   }
+
+   topics:  allBlogTag{
+    edges{
+      node{
+				title        
+      }
+    }
+  }
 }
 
 </page-query>
 <script>
 import PostListItem from "~/components/custom/Cards/PostListItem.vue";
 import Pagination from "~/components/custom/Pagination.vue";
+import NewsFilterHeader from "~/components/custom/NewsFilterHeader.vue";
 
 export default {
+  data() {
+    const allMonths = [
+      "All",
+      "January",
+      "February",
+      "March",
+      "April",
+      "May",
+      "June",
+      "July",
+      "August",
+      "September",
+      "October",
+      "November",
+      "December",
+    ];
+    const currYear = new Date().getFullYear();
+    var years = ["All"]
+    var r  = this.range(2019, currYear)
+    r.forEach((year) => years.push(String(year)));
+
+  return {
+      selectedTopic: "All",
+      selectedYear: "All",
+      selectedMonth: "All",
+      months: allMonths,
+      years: years,
+      listArchive: false,
+      archiveButtonText : "Archive"
+    };
+  },
+
+  metaInfo: {
+    title: "Newsroom",
+  },
   components: {
     PostListItem,
-    Pagination
+    Pagination,
+    NewsFilterHeader,
+  },
+  methods: {
+    setTopic: function (topic) {
+      this.selectedTopic = topic;
+    },
+    setYear(year) {
+      this.selectedYear = year;
+    },
+    setMonth(month) {
+      this.selectedMonth = month;
+    },
+    resetAll() {
+      this.selectedTopic = "All";
+      this.selectedYear = "All";
+      this.selectedMonth = "All";
+    },
+    range(start, end, step) {
+        var range = [];
+        var typeofStart = typeof start;
+        var typeofEnd = typeof end;
+
+        if (step === 0) {
+            throw TypeError("Step cannot be zero.");
+        }
+
+        if (typeofStart == "undefined" || typeofEnd == "undefined") {
+            throw TypeError("Must pass start and end arguments.");
+        } else if (typeofStart != typeofEnd) {
+            throw TypeError("Start and end arguments must be of same type.");
+        }
+
+        typeof step == "undefined" && (step = 1);
+
+        if (end < start) {
+            step = -step;
+        }
+
+        if (typeofStart == "number") {
+
+            while (step > 0 ? end >= start : end <= start) {
+                range.push(start);
+                start += step;
+            }
+
+        } else if (typeofStart == "string") {
+
+            if (start.length != 1 || end.length != 1) {
+                throw TypeError("Only strings with one character are supported.");
+            }
+
+            start = start.charCodeAt(0);
+            end = end.charCodeAt(0);
+
+            while (step > 0 ? end >= start : end <= start) {
+                range.push(String.fromCharCode(start));
+                start += step;
+            }
+
+        } else {
+            throw TypeError("Only string and number types are supported");
+        }
+
+        return range;
+
+    }
   },
   computed: {
-    
+
+    topics: function () {
+      var res = ["All"];
+      this.$page.topics.edges.forEach((edge) => res.push(edge.node.title));
+      return res;
+    },    
     contentHeight() {
       if (process.isClient) {
         return window.innerHeight - 100;
       }
     },
-  },
+
+
+  blogs() {
+      var res = {};
+      var old = this.$page.entries;
+      res.totalCount = old.totalCount;
+      res.pageInfo = old.pageInfo;
+      res.edges = [];
+
+      for (var i = 0; i < old.edges.length; i++) {
+        var node = old.edges[i].node;
+        
+        // Now check topic
+        var topics = ["All"];
+        node.tags.forEach((tag) => topics.push(tag.title));
+        
+        var nodeDate = new Date(node.datetime);
+        if (!topics.includes(this.selectedTopic)) continue;
+
+        // Check year
+        var years = ["All", String(nodeDate.getFullYear())];
+      
+        if (!years.includes(this.selectedYear)) continue;
+
+// Check Month
+        var months = ["All", this.months[nodeDate.getMonth() + 1]];
+        if (!months.includes(this.selectedMonth)) continue;
+        res.edges.push({ node: node, id: node.id });
+      }
+      return res;
+    },
+      },
+
 };
 </script>
--- src/pages/News.vue
+++ src/pages/News.vue
@@ -105,13 +105,16 @@ export default {
       "December",
     ];
     const currYear = new Date().getFullYear();
+    var years = ["All"]
+    var r  = this.range(2019, currYear)
+    r.forEach((year) => years.push(String(year)));
 
     return {
       selectedTopic: "All",
-      selectedYear: String(new Date().getFullYear()),
+      selectedYear: "All",
       selectedMonth: "All",
       months: allMonths,
-      years: ["All", String(currYear), String(currYear - 1)],
+      years:  years,
       listArchive: false,
       archiveButtonText : "Archive"
     };
@@ -156,6 +159,55 @@ export default {
         this.archiveButtonText = "Archive"
       }
     },
+    range(start, end, step) {
+        var range = [];
+        var typeofStart = typeof start;
+        var typeofEnd = typeof end;
+
+        if (step === 0) {
+            throw TypeError("Step cannot be zero.");
+        }
+
+        if (typeofStart == "undefined" || typeofEnd == "undefined") {
+            throw TypeError("Must pass start and end arguments.");
+        } else if (typeofStart != typeofEnd) {
+            throw TypeError("Start and end arguments must be of same type.");
+        }
+
+        typeof step == "undefined" && (step = 1);
+
+        if (end < start) {
+            step = -step;
+        }
+
+        if (typeofStart == "number") {
+
+            while (step > 0 ? end >= start : end <= start) {
+                range.push(start);
+                start += step;
+            }
+
+        } else if (typeofStart == "string") {
+
+            if (start.length != 1 || end.length != 1) {
+                throw TypeError("Only strings with one character are supported.");
+            }
+
+            start = start.charCodeAt(0);
+            end = end.charCodeAt(0);
+
+            while (step > 0 ? end >= start : end <= start) {
+                range.push(String.fromCharCode(start));
+                start += step;
+            }
+
+        } else {
+            throw TypeError("Only string and number types are supported");
+        }
+
+        return range;
+
+    }
 
 
   },
