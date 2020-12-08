06accd089d1471e2c4dc495e4fb78d65cded5675
samaradel <samar@incubaid.com>
2020-12-08 11:46:44 +0200

    Fix dropdown menu

--- src/components/custom/TagFilterHeader.vue
+++ src/components/custom/TagFilterHeader.vue
@@ -61,9 +61,7 @@
                     :key="tag.path"
                     class="cursor-pointer block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark-:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                     :href="tag.path"
-                     @click.self="
-                      open = false;
-                    "
+                    @click.self="open = false"
                     >{{ tag.title }}</a
                   >
                 </div>
@@ -88,29 +86,29 @@
 
 export default {
   props: ["tags", "selected"],
-  data(){
+  data() {
     return {
       tag: "All",
       isOpen: false,
       open: false,
       active: null,
-    }
+    };
   },
 
-methods: {
+  methods: {
     setActive(index) {
       this.active = index;
       this.open = !this.open;
     },
     close(e) {
-      this.open = false;
       if (!this.$el.contains(e.target)) {
         this.open = false;
       }
     },
   },
-  
-  
+  mounted() {
+    document.addEventListener("click", this.close);
+  },
   beforeDestroy() {
     document.removeEventListener("click", this.close);
   },
