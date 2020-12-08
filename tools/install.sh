64a465a7ed73f49cdbb032fdbd396f759f3b422f
kristof <kristof@incubaid.com>
2020-12-08 17:43:19 +0100

    installer

--- tools/install.sh
+++ tools/install.sh
@@ -1,4 +1,5 @@
-set -ex
+set -e
+set +x
 
 # INSTALL CRYSTAL TOOLS
 /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/crystaluniverse/crystaltools/master/tools/install.sh)"
@@ -8,6 +9,7 @@ ct git pull -u git@github.com:threefoldfoundation/www_threefold_farming.git
 ct git pull -u git@github.com:threefoldfoundation/data_threefold.git
 
 # GO INTO CODE DIERCTORY
+set -e
 cd ~/code/github/threefoldfoundation/www_threefold_farming
 
 # INSTALL GRIDSOME & DEPENDENCIES FOR GRIDSOME
@@ -15,12 +17,12 @@ rm -f yarn.lock
 rm -rf node_modules
 rm -rf .cache
 npm install --global @gridsome/cli
-#yarn install
+
+set +e
 npm install
 
 # RUN THE WEBSITE
 gridsome develop
-
 
 #to call this
 #/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/threefoldfoundation/www_threefold_farming/master/tools/install.sh)"
