15adbb74b1cb23ba5c0db79f4e9303ebb0fda96e
hamdy <hamdy.a.farag@gmail.com>
2020-12-09 14:42:11 +0200

    update installers

--- tools/install.sh
+++ tools/install.sh
@@ -2,11 +2,17 @@ set -e
 set +x
 
 # INSTALL CRYSTAL TOOLS
-/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/crystaluniverse/crystaltools/master/tools/install.sh)"
+sudo /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/crystaluniverse/crystaltools/master/tools/install.sh)"
+sudo chmod 777 /usr/local/bin/ct
 
 # GET REQUIRED REPOSITORIES
 ct git pull -u git@github.com:threefoldfoundation/www_threefold_farming.git
 ct git pull -u git@github.com:threefoldfoundation/data_threefold.git
+
+ln -s ~/code/github/threefoldfoundation/data_threefold/content/blog ~/code/github/threefoldfoundation/www_threefold_farming/content/blog
+ln -s ~/code/github/threefoldfoundation/data_threefold/content/person ~/code/github/threefoldfoundation/www_threefold_farming/content/person
+ln -s ~/code/github/threefoldfoundation/data_threefold/content/news ~/code/github/threefoldfoundation/www_threefold_farming/content/news
+ln -s ~/code/github/threefoldfoundation/data_threefold/content/project ~/code/github/threefoldfoundation/www_threefold_farming/content/project
 
 # GO INTO CODE DIERCTORY
 set -e
