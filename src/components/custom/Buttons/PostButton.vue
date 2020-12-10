3625cf437046e86d018f9d3bc79baff198d47677
samaradel <samar@incubaid.com>
2020-12-10 16:38:02 +0200

    Init PostBtn component

---
+++ src/components/custom/Buttons/PostButton.vue
@@ -0,0 +1,11 @@
+<template>
+    <div class="mt-8">
+        <a :href="card.link" class="bg-gray-900 learn-button hover:bg-gray-700 text-gray-100 px-5 py-3 font-semibold rounded">{{ card.button }}</a>
+    </div>
+</template>
+
+<script>
+export default {
+    props:['card']
+}
+</script>
