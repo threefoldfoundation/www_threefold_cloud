5fd3949735a7a70200c7d7f6e912f3b0e6fae9c9
Vilnite <vilnitekristine@gmail.com>
2021-02-17 10:58:44 +0200

    updated tab

--- gridsome.config.js
+++ gridsome.config.js
@@ -5,7 +5,7 @@
 // To restart press CTRL + C in terminal and run `gridsome develop`
 
 module.exports = {
-    siteName: 'Threefold Webstack',
+    siteName: 'ThreeFold Cloud',
     plugins: [
 
         // Local models
