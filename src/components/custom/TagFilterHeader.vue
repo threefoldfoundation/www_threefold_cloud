3358bbcd54c0dc3f947201c33de6d814267b3887
hamdy <hamdy.a.farag@gmail.com>
2021-03-07 13:03:59 +0200

    work with pathPrefixes and without

--- src/components/custom/TagFilterHeader.vue
+++ src/components/custom/TagFilterHeader.vue
@@ -65,7 +65,7 @@
                     v-for="tag in tags"
                     :key="tag.path"
                     class="cursor-pointer block px-4 py-2 mt-2 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark-:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
-                    :href="tag.path"
+                    :href="$url(tag.path)"
                     @click.self="open = false"
                     >{{ tag.title }}</a
                   >
