3975f0de0619ab2e9c68b047b44347b7fc99121f
hamdy <hamdy.a.farag@gmail.com>
2021-04-08 14:55:57 +0200

    Revert "fix"
    This reverts commit e1820908f175684664db3c1128bc2240c37ebf54.

--- src/components/custom/Navbar/Navbar.vue
+++ src/components/custom/Navbar/Navbar.vue
@@ -163,7 +163,7 @@
                     v-else
                     class="block px-4 py-1 text-sm font-semibold bg-transparent rounded-lg dark:bg-transparent dark:hover:bg-gray-600 dark-:focus:bg-gray-600 dark:focus:text-white dark:hover:text-white dark:text-gray-200 md:mt-0 hover:text-gray-900 focus:text-gray-900 hover:bg-gray-200 focus:bg-gray-200 focus:outline-none focus:shadow-outline"
                     @click="open = false"
-                    :href="link.path"
+                    :href="$url(link.path)"
                     >{{ link.title }}</a
                   >
                 </div>
